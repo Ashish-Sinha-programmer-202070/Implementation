9056e4eee444fab932631b731e78ddc161786dd2
Ashish Sinha <84274841+Ashish-Sinha-programmer-202070@users.noreply.github.com>
2024-02-19 19:10:12 +0530

    Finance Dashboard

---
+++ Finance Dashboard.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 19:16:06 2024
+
+@author: ASHISH SINHA
+"""
+
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+st.title('Lit Finance Dashboard')
